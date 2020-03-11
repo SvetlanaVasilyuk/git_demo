f83351e42fb292fd0596690b29d147278519fa6b
Svetlana Vasilyuk <lanarusufa@gmail.com>
2020-03-12 01:54:23 +0300

    Changes in two classes

--- src/com/github/svtest/Cat.java
+++ src/com/github/svtest/Cat.java
@@ -1,4 +1,7 @@
 package com.github.svtest;
 
 public class Cat {
+    public void talk(){
+        System.out.println("Meow");
+    }
 }
--- src/com/github/svtest/Main.java
+++ src/com/github/svtest/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("First version of code is here");
+        System.out.println("New string has been added in the second commit");
     }
 }
